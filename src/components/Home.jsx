b5e206cefcae0207ad074d5bfe57556b37299d3c
Marco Solari <m.solari@se20.qmul.ac.uk>
2023-08-03 17:43:32 +0100

    styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,15 +5,15 @@ const Home = () => {
     <div id='home' className='w-full h-screen'>
       {/* Container */}
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
+        <h1 className='text-4xl sm:text-7xl font-bold text-[#3e4d6a]'>
           Course Curator
         </h1>
-        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
+        <h2 className='text-4xl sm:text-7xl font-bold text-[#5f81b1]'>
           The one stop shop for your personal development
         </h2>
         <div>
          <a href='courses'>
-          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
+          <button className='enter'>
             Enter App
           </button>
          </a>
